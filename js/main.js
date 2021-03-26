79412074b853d98d5dc258cd1e243d5a159ce7ef
Angello Ordoñez <angelo-mjz7@hotmail.com>
2021-03-26 18:37:22 -0500

    menu y backfround hero

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
+// const menuToggle = document.querySelector('.toggle');
+// const showcase = document.querySelector('.showcase');
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-})
+// menuToggle.addEventListener('click', () => {
+//   menuToggle.classList.toggle('active');
+//   showcase.classList.toggle('active');
+// })
+const navMain = document.getElementById('nav_header_main');
+
+window.addEventListener('scroll',e =>{
+	const scrollY = window.scrollY;
+	if(scrollY <= 75){
+		navMain.style.top = `${75 - scrollY}px`;
+		navMain.classList.remove('sticky');
+	}else{
+		navMain.style.top = `0`;
+		navMain.classList.add('sticky');
+	}
+});
