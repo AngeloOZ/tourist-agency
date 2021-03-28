a42830005c0f523ae7c97573be02872ba9b190da
Angello Ordoñez <angelo-mjz7@hotmail.com>
2021-03-28 13:15:45 -0500

    implementacion de footer y testimonial

--- js/main.js
+++ js/main.js
@@ -17,3 +17,39 @@ window.addEventListener('scroll',e =>{
 		navMain.classList.add('sticky');
 	}
 });
+
+
+document.addEventListener('DOMContentLoaded', _ =>{
+	initCarouselTestimonial();
+	
+})
+
+function initCarouselTestimonial(){
+	const testCont = document.getElementById('sub_cont_test');
+	const contIndicadores = document.getElementById('indicadores_test');
+	if(testCont){
+		const numTestimoniales = testCont.childElementCount;
+		testCont.style.gridTemplateColumns = `repeat(${numTestimoniales},100%)`
+		const fragment = document.createDocumentFragment();
+		for (let i = 0; i < numTestimoniales; i++) {
+			const span = document.createElement('SPAN');
+			span.classList.add('indicador');
+			span.setAttribute('num_indicador', i);
+			fragment.appendChild(span);
+		}
+		contIndicadores.appendChild(fragment);
+		contIndicadores.firstElementChild.classList.add('active')
+		contIndicadores.addEventListener('click', e =>{
+			if(e.target.hasAttribute('num_indicador') && e.target.classList.contains('indicador')){
+				const child = contIndicadores.childNodes;
+				child.forEach(element => {
+					element.classList.remove('active')
+				});
+				e.target.classList.add('active')
+				const numIndicador = parseInt(e.target.getAttribute('num_indicador'));
+				testCont.style.transform = `translateX(-${numIndicador * 100}%)`;
+			}
+		})
+	}
+
+}
